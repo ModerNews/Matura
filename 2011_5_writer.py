28d09cae915d7546dca0d4dd9fdd844f639e2b6f
ModerNews <grzesiujag@poczta.fm>
2022-01-30 22:56:47 +0100

    Automatyczne generowanie tabeli

--- 2011_5_writer.py
+++ 2011_5_writer.py
@@ -6,12 +6,15 @@ cur = mydb.cursor()
 with open("2011_res\\osoby.txt", "r") as file:
     data = file.read().split("\n")[:-1]
 
+cur.execute('CREATE TABLE "osoby" ("id_osoby" TEXT,"imie" TEXT,"nazwisko" TEXT,"tel" TEXT)')
+
 for line in data:
     line = line.split(";")
     cur.execute("INSERT INTO osoby VALUES ('{}', '{}', '{}', '{}')".format(line[0], line[1], line[2], line[3]))
 
 mydb.commit()
 
+cur.execute('CREATE TABLE "psy" ("id_psa" INTEGER, "rasa" TEXT, "wiek" INTEGER, "plec" TEXT, "medale" INTEGER, "id_osoby" TEXT)')
 with open("2011_res\\psy.txt", "r") as file:
     data = file.read().split("\n")[:-1]
 
