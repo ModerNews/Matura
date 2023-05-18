1c75c92458c29da1ed3be74f3dd6573c4e7ad55e
Gruzin <grzegorz.jagielski.2019@zsa.pwr.edu.pl>
2023-05-18 15:52:33 +0200

    Conways

--- Algos/conways.py
+++ Algos/conways.py
@@ -6,26 +6,27 @@ start_set = "xxxxxxxxxx\n" \
             "xoooxxoxxo\n" \
             "xxxxxxxxxx"
 
-data = [list(line) for line in start_set.split('\n')]
-print(data)
-
-generations = [data]
+generations = [[list(line) for line in start_set.split('\n')]]
 
 for i in range(100):
-    new_gen = deepcopy(generations[-1])
+    new_gen, data = deepcopy(generations[-1]), generations[-1]
     for x in range(len(data[0])):
         for y in range(len(data)):
-            neighbors = [data[y-1][x-1], data[y-1][x], data[y][x+1 % len(data[0])],
-                         data[y][x-1], data[y][x+1 % len(data[0])],
-                         data[y+1 % len(data)][x-1], data[y+1 % len(data)][x], data[y+1 % len(data)][x+1 % len(data[0])]]
+            neighbors = [data[y-1][x-1], data[y-1][x], data[y-1][(x+1) % len(data[0])],
+                         data[y][x-1], data[y][(x+1) % len(data[0])],
+                         data[(y+1) % len(data)][x-1], data[(y+1) % len(data)][x], data[(y+1) % len(data)][(x+1) % len(data[0])]]
             if neighbors.count('o') < 2:
-                new_gen[x][y] = 'x'
-            elif data[x][y] == 'o' and (2 <= neighbors.count('o') <= 3):
-                new_gen[x][y] = 'o'
-            elif data[x][y] == 'x' and neighbors.count('o') == 3:
-                new_gen[x][y] = 'o'
+                new_gen[y][x] = 'x'
+            elif data[y][x] == 'o' and (2 <= neighbors.count('o') <= 3):
+                new_gen[y][x] = 'o'
+            elif data[y][x] == 'x' and neighbors.count('o') == 3:
+                new_gen[y][x] = 'o'
             else:
-                new_gen[x][y] = 'x'
+                new_gen[y][x] = 'x'
     generations.append(deepcopy(new_gen))
 
-print(generations)
+i = 1
+for gen in generations:
+    print(f"Generation: {i}")
+    print('\n'.join([' '.join(line) for line in gen]))
+    i += 1
--- Projects/rafisto_nb.py
+++ Projects/rafisto_nb.py
@@ -11,15 +11,21 @@ diff_change = [diffs[i - 1] / diffs[i] for i in range(1, len(diffs))]
 diff_change = sum(diff_change) / len(diff_change)
 print(diff_change)
 
-while data[-1] < 186*28:
+while data[-1] < 366*28:
     diff = diffs[-1] * (1 + diff_change)
     diffs.append(diff)
     data.append(data[-1] + diff)
+
+z = np.polyfit(list(range(len(data))), data, 3)
+p = np.poly1d(z)
+
+print(p(list(range(len(data)))))
 
 ax = plt.axes()
 ax.set_facecolor('xkcd:light grey')
 
 plt.plot([f"{start_year + i}/{start_year + i + 1}" for i in range(len(data))], data, figure=plt.figure(figsize=(19, 10)), color='blue')
+plt.plot([f"{start_year + i}/{start_year + i + 1}" for i in range(len(data))], p(list(range(len(data)))), color='red', linestyle='--')
 plt.yticks([150 + i * 250 for i in range(0, math.floor(data[-1] / 250))])
 plt.grid(True)
 plt.scatter([f"{start_year + i}/{start_year + i + 1}" for i in range(len(data))], data, color='orange')
@@ -30,5 +36,6 @@ plt.xlabel('Rok szkolny')
 plt.title('Przewidywana liczba godzin nieobecności Rafist0 w następnych latach')
 plt.annotate(f'Ilość godzin w roku szkolnym: {186*28}', (0, 186*28))
 plt.plot(list(range(len(data))), [186*28 for i in range(len(data))], color='orange', linestyle='--')
+plt.plot(list(range(len(data))), [365*28 + 365*28*0.1 for i in range(len(data))], color='purple', linestyle='--')
 # plt.plot(data[idx], 186*28, 'ro')
 plt.show()
